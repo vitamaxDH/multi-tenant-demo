57988028ace9cb82817fed67f2e7f6547521c7d5
Max <hellonewtry@gmail.com>
2023-06-11 13:48:28 +0900

    add: query dsl config for entityManager

---
+++ src/main/kotlin/io/vitamax/singleschematenantdemo/infra/QueryDslConfig.kt
@@ -0,0 +1,18 @@
+package io.vitamax.singleschematenantdemo.infra
+
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import jakarta.persistence.EntityManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class QueryDslConfig(
+    private val em: EntityManager,
+){
+
+    @Bean
+    fun jpaQueryFactory(): JPAQueryFactory {
+        return JPAQueryFactory(em)
+    }
+}
